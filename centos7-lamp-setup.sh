ed2a7989b496893a55ca76d16412a803b12a5fe0
mitchellbryant <38734085+mitchellbryant@users.noreply.github.com>
2018-06-28 17:06:02 -0400

    added some flags

--- centos7-lamp-setup.sh
+++ centos7-lamp-setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 # 
 # For CentOS7
-# This script will update CentOS7, set up LAMP stack with PHP 7.030, mariadb, and apache
-# 
+# Will update CentOS7, set up LAMP stack with PHP 7.030, mariadb 5.5.56, and apache
+# Make sure you have Git installed
 # INSTRUCTIONS:
-# 1. Install Git if you dont have it already with the below command:
-# 		sudo yum -y install git
-# 2. Copy this shell script with the below command:
-# 		sudo git clone https://github.com/mitchellbryant/centos7-lamp
-# 3. Change to the cloned directory
-#		cd centos7-lamp
-# 4. Make the script executable with the following command: 
-#      sudo chmod a+x centos7-lamp-setup.sh
-# 5. Execute the script as a sudo user:
+# 1. Copy this shell script to your home directory or the /tmp directory.
+# 2. Make it executable with the following command: 
+#      chmod a+x centos7-lamp-setup.sh
+# 3. Execute the script as a sudo user:
 #      sudo ./centos7-lamp-setup.sh
 
 #The following will install Apache, MariaDB, and allow web traffic
@@ -39,18 +34,18 @@ sudo systemctl restart firewalld
 
 #This will now install PHP 7
 
-sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
-sudo yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 
-sudo yum install yum-utils
+sudo yum -y install yum-utils
 
 yum-config-manager --enable remi-php70
 #If you want to install PHP 7.1 or PHP 7.2 on CentOS 7, just enable it as shown below
 # yum-config-manager --enable remi-php71    for 7.1 
 # yum-config-manager --enable remi-php72    for 7.2
 
-sudo yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-mbstring php-xml
+sudo yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-mbstring php-xml
 
 sudo systemctl restart httpd.service
 
